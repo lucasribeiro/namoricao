662abf0d9c867090a996a47144845a3ad0b05844
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2023-10-09 13:28:21 -0300

    Tela de login ajustes

--- app/src/main/java/com/namoricao/app/ui/Login.kt
+++ app/src/main/java/com/namoricao/app/ui/Login.kt
@@ -2,6 +2,7 @@ package com.namoricao.app.ui
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -11,6 +12,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import com.namoricao.app.R
 import android.widget.Button
 import com.namoricao.app.databinding.ActivityLoginBinding
+import org.json.JSONObject
 
 class Login : AppCompatActivity() {
 
@@ -21,19 +23,49 @@ class Login : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val buttonLogin = findViewById<Button>(R.id.loginButton)
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
 
         buttonLogin.setOnClickListener {
-            // Obter os valores dos campos de login e senha
-            //val login = binding.editTextLogin.text.toString()
-            //val senha = binding.editTextPassword.text.toString()
+            val emailDigitado = binding.emailEditText.text.toString()
+            val senhaDigitada = binding.senhaEditText.text.toString()
 
-            // Você pode adicionar validações de login e senha aqui
-            // Por exemplo, verificar se o login e a senha são válidos
+            val jsonContent = resources.openRawResource(R.raw.users).bufferedReader().use {
+                it.readText()
+            }
 
-            // Se a validação for bem-sucedida, você pode iniciar a próxima atividade
-            //val intent = Intent(this, MainActivity::class.java)
-            //startActivity(intent)
+            try {
+                val jsonObject = JSONObject(jsonContent)
+                val usuariosArray = jsonObject.getJSONArray("usuarios")
+
+                var credenciaisValidas = false
+
+                for (i in 0 until usuariosArray.length()) {
+                    val usuario = usuariosArray.getJSONObject(i)
+                    val email = usuario.getString("email")
+                    val senha = usuario.getString("senha")
+
+                    if (email == emailDigitado && senha == senhaDigitada) {
+                        // As credenciais são válidas, você pode fazer o que for necessário aqui
+                        // Por exemplo, iniciar uma nova atividade ou exibir uma mensagem de sucesso
+                        // Após a autenticação bem-sucedida.
+                        credenciaisValidas = true
+                        break
+                    }
+                }
+
+                if (!credenciaisValidas) {
+                    // Se as credenciais não correspondem a nenhum usuário no arquivo JSON,
+                    // exiba um Toast com a mensagem de erro.
+                    Toast.makeText(this, "E-mail ou senha inválido", Toast.LENGTH_SHORT).show()
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
+    }
+
+    // Método para fechar o aplicativo
+    fun fecharApp(view: View) {
+        finish() // Fecha a atividade atual, o que também encerra o aplicativo
     }
 }
