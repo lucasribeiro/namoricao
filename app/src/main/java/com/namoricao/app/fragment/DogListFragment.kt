356fb0fee416e54309cc12f104452f323e9ca3ec
Lucas Luis Ribeiro <lucas.luisribeiro@gmail.com>
2023-10-24 21:30:53 -0300

    Listagem de meus cães, ajustes

--- app/src/main/java/com/namoricao/app/fragment/DogListFragment.kt
+++ app/src/main/java/com/namoricao/app/fragment/DogListFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.namoricao.app.viewmodel.DogListViewModel
 import com.namoricao.app.R
+import com.namoricao.app.adaptors.DogItemDecoration
 import com.namoricao.app.adaptors.DogListAdapter
 import com.namoricao.app.model.Dog
 
@@ -28,10 +30,18 @@ class DogListFragment : Fragment() {
         val view = inflater.inflate(R.layout.fragment_dog_list, container, false)
 
         val recyclerView = view.findViewById<RecyclerView>(R.id.recyclerViewDogs)
+        val spacingInPixels = resources.getDimensionPixelSize(R.dimen.spacing_between_items) // 20 pixels
+        val dividerHeightInPixels = resources.getDimensionPixelSize(R.dimen.divider_height) // Altura da linha, em pixels
+        val dividerColor = ContextCompat.getColor(requireContext(), R.color.divider_color) // Cor da linha
+        recyclerView.addItemDecoration(DogItemDecoration(spacingInPixels, dividerHeightInPixels, dividerColor))
+
         val dogList = listOf(
             Dog("Cachorro 1", "Raça 1", R.drawable.logo),
             Dog("Cachorro 2", "Raça 2", R.drawable.logo),
-            Dog("Cachorro 3", "Raça 3", R.drawable.logo)
+            Dog("Cachorro 3", "Raça 3", R.drawable.logo),
+            Dog("Cachorro 1", "Raça 4", R.drawable.logo),
+            Dog("Cachorro 2", "Raça 5", R.drawable.logo),
+            Dog("Cachorro 3", "Raça 6", R.drawable.logo)
             // Adicione mais cães conforme necessário
         )
 
--- app/src/main/java/com/namoricao/app/ui/MainActivity.kt
+++ app/src/main/java/com/namoricao/app/ui/MainActivity.kt
@@ -7,9 +7,11 @@ import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 import com.namoricao.app.R
+import com.namoricao.app.fragment.DogListFragment
 import com.namoricao.app.fragment.PrincipalFragment
 
 class MainActivity : AppCompatActivity() {
@@ -38,6 +40,8 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.setHomeButtonEnabled(true)
         supportActionBar?.setHomeAsUpIndicator(R.drawable.menu)
 
+        setupNavigationView()
+
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, PrincipalFragment())
@@ -51,41 +55,29 @@ class MainActivity : AppCompatActivity() {
         return true
     }
 
-    // Lidar com as ações do menu
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    private fun setupNavigationView() {
 
-        when (item.itemId) {
-            R.id.menu_buscar -> {
-                // Lógica para ação "Buscar"
-                return true
-            }
-            R.id.menu_meus_caes -> {
-                Toast.makeText(this, "Email inválido", Toast.LENGTH_SHORT).show()
-                return true
-                /*
-                val navigationView = findViewById<NavigationView>(R.id.nav_view)
-                navigationView.setNavigationItemSelectedListener { menuItem ->
-                    when (menuItem.itemId) {
-                        R.id.nav_my_dogs -> {
-                            val fragmentManager = supportFragmentManager
-                            val transaction = fragmentManager.beginTransaction()
-                            transaction.replace(R.id.fragment_container, DogListFragment())
-                            transaction.commit()
-                        }
-                        // Outros itens de menu
-                    }
-                    val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
-                    drawer.closeDrawer(GravityCompat.START)
-                    true
+        val navigationView = findViewById<NavigationView>(R.id.navigation_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu_meus_caes -> {
+                    val fragmentManager = supportFragmentManager
+                    val transaction = fragmentManager.beginTransaction()
+                    transaction.replace(R.id.container, DogListFragment())
+                    transaction.commit()
                 }
+                R.id.menu_buscar -> {
+                    Toast.makeText(this, "menu_buscar", Toast.LENGTH_SHORT).show()
+                }
+                R.id.menu_sair -> {
+                    Toast.makeText(this, "Sair", Toast.LENGTH_SHORT).show()
+                }
+                // Outros itens de menu
+            }
 
-                 */
-            }
-            R.id.menu_sair -> {
-                // Lógica para ação "Sair"
-                return true
-            }
-            else -> return super.onOptionsItemSelected(item)
+            val drawer = findViewById<DrawerLayout>(R.id.drawer_layout)
+            drawer.closeDrawer(GravityCompat.START)
+            true
         }
     }
 }
